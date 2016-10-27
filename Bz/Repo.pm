7d41a6246f74706af2c9c706f41542e9d7a46ee3
David Lawrence <dkl@mozilla.com>
2016-10-27 10:42:18 +0000

    Ignore local/lib/perl5 and Makefile when checking for tabs

--- Bz/Repo.pm
+++ Bz/Repo.pm
@@ -329,6 +329,7 @@ sub should_ignore_file {
         js/yui3/
         template_cache/
         tmp/
+        local/lib/perl5/
     );
     my @ignore_suffix = qw(
         .orig
@@ -358,6 +359,7 @@ sub check_for_tabs {
     my @ignore = qw(
         js/change-columns.js
         t/002goodperl.t
+        Makefile
     );
     find(sub {
             my $file = $File::Find::name;
